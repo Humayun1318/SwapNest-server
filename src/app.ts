effc7907d85b56b9654cc7664211d2798da8da48
Humayun1318 <humayunKabir6267@gmail.com>
2025-04-15 22:57:25 +0600

    folder structure basic function added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -9,17 +11,15 @@ app.use(express.json());
 
 // application routes
 // app.use('/api/v1', router);
-app.use('/api/v1', (req: Request, res: Response) => {
-  res.send('Hi Team Runtime Nation !');
-});
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi Team Runtime Nation !');
 });
 
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
-//Not Found
-// app.use(notFound);
+// Not Found
+app.use(notFound);
 
 export default app;
